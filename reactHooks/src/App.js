5680bf59f138d6c75581c8dade5d37376c9f2a30
maxyee <eyaminkhan00@gmail.com>
2020-04-30 21:56:09 +0600

    React hooks concept project and code added

---
+++ reactHooks/src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import IntroHooks from './components/IntroHooks';
+
+function App() {
+
+  return (
+    <div className="App">
+      <div>
+        <p>Inroduction Hook with Click Event</p>
+        <hr></hr>
+        <IntroHooks></IntroHooks>
+        <hr></hr>
+      </div>
+
+      <div>
+        
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ reactHooks/src/components/IntroHooks.js
@@ -0,0 +1,19 @@
+import React, { useState } from 'react';
+
+
+
+function IntroHooks(){
+
+    const [count, setCount] = useState(0);
+
+    return(
+        <div>
+            <p>You clicked {count} times</p>
+            <button onClick={()=> setCount(count + 1)}>
+                Click me
+            </button>
+        </div>
+    );
+}
+
+export default IntroHooks;
